12e10683b1020ad699f11306cc1721282b9d187c
Robert Greathouse <robbykap@byu.edu>
2024-09-12 13:18:24 -0600

    Finished BishopMoves

--- shared/src/main/java/chess/moves/BishopMoves.java
+++ shared/src/main/java/chess/moves/BishopMoves.java
@@ -9,6 +9,14 @@ import java.util.Collection;
 public class BishopMoves {
 
     public static Collection<ChessMove> getMoves(ChessBoard board, ChessPosition myPosition) {
-        return null;
+
+        int[][] directions = {
+            {1, 1},    // up-right
+            {1, -1},   // up-left
+            {-1, 1},   // down-right
+            {-1, -1}   // down-left
+        };
+
+        return FindMoves.getMoves(board, myPosition, directions);
     }
 }
