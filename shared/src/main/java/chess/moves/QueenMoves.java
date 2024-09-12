d5f86d55790d99863610ea556c26af88d51ac7c2
Robert Greathouse <robbykap@byu.edu>
2024-09-12 13:19:03 -0600

    Finished QueenMoves

--- shared/src/main/java/chess/moves/QueenMoves.java
+++ shared/src/main/java/chess/moves/QueenMoves.java
@@ -9,6 +9,18 @@ import java.util.Collection;
 public class QueenMoves {
 
     public static Collection<ChessMove> getMoves(ChessBoard board, ChessPosition myPosition) {
-        return null;
+
+        int[][] directions = {
+                {1, 0},  // forward
+                {-1, 0}, // backward
+                {0, 1},  // right
+                {0, -1},  // left
+                {1, 1},  // forward right
+                {1, -1},  // forward left
+                {-1, 1},  // backward right
+                {-1, -1}  // backward left
+        };
+
+        return FindMoves.getMoves(board, myPosition, directions);
     }
 }
