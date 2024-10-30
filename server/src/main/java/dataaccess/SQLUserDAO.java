17eb28ed41438878662a5686056ef5bf6647a7d0
Robert Greathouse <robbykap@byu.edu>
2024-10-30 01:30:10 -0600

    Created and tested SQLUserDAO

---
+++ server/src/main/java/dataaccess/SQLUserDAO.java
@@ -0,0 +1,83 @@
+package dataaccess;
+
+import java.sql.*;
+
+import model.UserData;
+import org.mindrot.jbcrypt.BCrypt;
+
+
+public class SQLUserDAO implements UserDAO {
+
+    public SQLUserDAO() {
+        try {
+            configureDatabase();
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        try (var connection = DatabaseManager.getConnection()) {
+            var statement = connection.prepareStatement("SELECT * FROM UserData WHERE username = ?");
+            statement.setString(1, username);
+            var resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                return new UserData(
+                    resultSet.getString("username"),
+                    resultSet.getString("password"),
+                    resultSet.getString("email")
+                );
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("User not found");
+        }
+        return null;
+    }
+
+    @Override
+    public void createUser(UserData user) throws DataAccessException {
+        try (var connection = DatabaseManager.getConnection()) {
+            var statement = connection.prepareStatement("INSERT INTO UserData (username, password, email) VALUES (?, ?, ?)");
+            statement.setString(1, user.username());
+            statement.setString(2, BCrypt.hashpw(user.password(), BCrypt.gensalt()));
+            statement.setString(3, user.email());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException("User already exists");
+        }
+
+    }
+
+    @Override
+    public void clear() {
+        try (var connection = DatabaseManager.getConnection()) {
+            var statement = connection.prepareStatement("DELETE FROM UserData");
+            statement.executeUpdate();
+        } catch (SQLException | DataAccessException ignored) {
+        }
+    }
+
+    private final String[] createStatements = {
+        """
+        CREATE TABLE IF NOT EXISTS UserData (
+                        username VARCHAR(255) NOT NULL,
+                        password VARCHAR(255) NOT NULL,
+                        email VARCHAR(255) NOT NULL,
+                        PRIMARY KEY (username)
+        )"""
+    };
+
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var connection = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = connection.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Error when configuring database");
+        }
+    }
+}
