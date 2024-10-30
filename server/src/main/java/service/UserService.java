0d807c2da6922bae02e57ce6e316e3e381c7ce6e
Robert Greathouse <robbykap@byu.edu>
2024-10-30 02:10:12 -0600

    Reapply "Updated UserService to check for hashed passwords"
    This reverts commit c54e3570e024d34fb01a1e731a090c290309aca0.

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -9,6 +9,7 @@ import dataaccess.AlreadyTakenException;
 import dataaccess.BadRequestException;
 import dataaccess.UnauthorizedException;
 import dataaccess.DataAccessException;
+import org.mindrot.jbcrypt.BCrypt;
 
 import java.util.UUID;
 
@@ -49,7 +50,7 @@ public class UserService {
             UserData user = userDAO.getUser(username);
 
             // Check if the password is incorrect, throw an UnauthorizedException if it is not
-            if (user.password().equals(password)) {
+            if (BCrypt.checkpw(password, user.password())) {
 
                 // Create an AuthData and insert it into the AuthDAO
                 String authToken = UUID.randomUUID().toString();
